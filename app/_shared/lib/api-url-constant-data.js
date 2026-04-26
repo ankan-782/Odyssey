4bb11e7546f0c562d13801d5f69a4c4cb1fcc6d0
avijitroy-urelaa <avijitroy@dev.urelaa.com>
2026-04-26 19:56:09 +0600

    app base url changed

--- app/_shared/lib/api-url-constant-data.js
+++ app/_shared/lib/api-url-constant-data.js
@@ -2,7 +2,7 @@ export const ROOT_URL = "http://localhost:8000";
 export const IMAGE_URL = "http://localhost:8000/files";
 
 // App Base URL for internal API calls (server-side)
-export const APP_BASE_URL = "http://localhost:3000";
+export const APP_BASE_URL = process.env.NEXT_PUBLIC_APP_URL || "";
 
 export const GET_SITE_CONFIG_DATA_URL = `${ROOT_URL}/site-config`;
 export const GET_STATIC_PAGES_DATA_URL = `${ROOT_URL}/static-page/single`;
